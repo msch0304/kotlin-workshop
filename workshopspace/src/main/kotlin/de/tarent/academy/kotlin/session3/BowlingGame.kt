4010ea00f42766e7a3a8fb236a827cef95103bf3
Michael Schmidt <m.schmidt@tarent.de>
2018-07-18 11:06:37 +0200

    merged upstream bolinggame start

---
+++ workshopspace/src/main/kotlin/de/tarent/academy/kotlin/session3/BowlingGame.kt
@@ -0,0 +1,37 @@
+package de.tarent.academy.kotlin.session3
+
+/**
+ * Created by micha on 02.07.18.
+ */
+
+
+class BowlingGame {
+
+    var frames = arrayOf(Frame())
+
+    var score =
+
+    fun addRoles(pins:Int)  {
+        frames = frames + Frame()
+
+
+    }
+
+}
+
+class Frame {
+
+    val pinsRolled = intArrayOf(0,0)
+
+    var score = 0
+
+    fun addPin(pins:Int){
+        if (pinsRolled.)
+    }
+}
+
+
+
+class LastFrame : Frame() {
+
+}
